ff5c92319774e303e8d7f056f5da9541196f92a5
Matheus Albuquerque <matheusapf1@gmail.com>
2025-04-22 22:23:38 -0300

    Configuração inicial do projeto

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// Espera o DOM ser completamente carregado
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Documento carregado!');
+    
+    // Aqui adicionaremos mais funcionalidades posteriormente
+});
